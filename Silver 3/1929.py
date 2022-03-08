6b48a3996efc8c87d252d08c63ecd606920713f1
Lee Joo Yeol <dhy2457@naver.com>
2022-03-08 17:32:34 +0900

    Create 1929.py

---
+++ Silver 3/1929.py
@@ -0,0 +1,16 @@
+import math
+
+def isPrime(num):
+    if num == 1:
+        return False
+    else:
+        for i in range(2, int(math.sqrt(num))+1):
+            if num % i == 0:
+                return False
+        return True
+
+M, N = map(int, input().split()) #자연수 M이상 N이하의 소수를 구한다.
+
+for i in range(M, N+1):
+    if isPrime(i):
+        print(i)
