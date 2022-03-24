2bb9ca0d0de5e8c7fe3f281f23daadfcc4a4d543
Lee Joo Yeol <dhy2457@naver.com>
2022-03-24 16:08:09 +0900

    Create 11866.py

---
+++ Silver 4/11866.py
@@ -0,0 +1,19 @@
+import sys
+from collections import deque
+
+n, k = map(int,input().split())
+
+arr = list(range(1, n+1))
+arr = deque(arr)
+print("<", end='')
+
+
+while(arr):
+    for i in range(k-1):
+        arr.append(arr.popleft())
+    print(arr.popleft(), end='')
+
+    if arr:
+        print(", ", end='')
+
+print(">")
