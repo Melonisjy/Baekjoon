839060a92edb65638d3817432ec79ea17cb00ab7
Lee Joo Yeol <dhy2457@naver.com>
2022-03-25 23:39:11 +0900

    Create 2754.py

---
+++ Bronze 3/2754.py
@@ -0,0 +1,28 @@
+avg = str(input())
+
+if avg == 'A+':
+    print('4.3')
+elif avg == 'A0':
+    print('4.0')
+elif avg == 'A-':
+    print('3.7')
+elif avg == 'B+':
+    print('3.3')
+elif avg == 'B0':
+    print('3.0')
+elif avg == 'B-':
+    print('2.7')
+elif avg == 'C+':
+    print('2.3')
+elif avg == 'C0':
+    print('2.0')
+elif avg == 'C-':
+    print('1.7')
+elif avg == 'D+':
+    print('1.3')
+elif avg == 'D0':
+    print('1.0')
+elif avg == 'D-':
+    print('0.7')
+elif avg == 'F':
+    print('0.0')
