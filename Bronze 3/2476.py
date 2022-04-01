ebcab771b58cc317546cda7bad9d151a40b682a8
Lee Joo Yeol <dhy2457@naver.com>
2022-04-01 21:21:11 +0900

    Create 2476.py

---
+++ Bronze 3/2476.py
@@ -0,0 +1,17 @@
+answer = 0
+
+for i in range(int(input())):
+    a, b, c = map(int, input().split())
+
+    if a == b == c:
+        answer = max(answer, 10000 + a * 1000)
+    elif a == b:
+        answer = max(answer, 1000 + a * 100)
+    elif a == c:
+        answer = max(answer, 1000 + a * 100)
+    elif b == c:
+        answer = max(answer, 1000 + b * 100)
+    else:
+        answer = max(answer, 100 * max(a, b, c))
+
+print(answer)
