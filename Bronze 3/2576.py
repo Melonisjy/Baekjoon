e6e41238643d993e2496e789a8fb3aac48527e40
Melonisjy <dhy2457@naver.com>
2022-11-15 10:52:06 +0900

    create 2576.py

---
+++ Bronze 3/2576.py
@@ -0,0 +1,22 @@
+lst = []
+even = [] # 짝수
+odd = [] # 홀수
+cnt = 0
+
+for i in range(7):
+    lst.append(int(input()))
+
+for i in range(len(lst)):
+    if lst[i] % 2 == 0: # 홀수이면
+        even.append(lst[i])
+        cnt += 1
+    else:
+        odd.append(lst[i])
+
+
+if cnt == 7:
+    print('-1')
+else:
+    print(sum(odd))
+    print(min(odd))
+
